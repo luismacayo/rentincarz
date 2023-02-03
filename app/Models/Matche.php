63194cfc40b13ee152e26b5f890238292d6f5eb8
Luis Macayo <luismacayo@gmail.com>
2023-02-03 17:27:58 -0500

    updated: fechas de partidos

--- app/Models/Matche.php
+++ app/Models/Matche.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
@@ -13,6 +14,7 @@ class Matche extends Model
 
     protected $fillable = [
         'match_id',
+        'match_date',
         'hometeam_name',
         'hometeam_shortname',
         'hometeam_tla',
@@ -24,6 +26,31 @@ class Matche extends Model
         'winner',
         'duration',
     ];
+
+        /**
+     * Interact with the user's first name.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function matchDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value,
+            set: fn ($value) => Carbon::parse($value)->toDateTimeString(),
+        );
+    }
+
+    public static function getNextDayMatches()
+    {
+        $matches = self::next(1)->get();
+        return $matches;
+    }
+    
+    public static function getNextWeekMatches()
+    {
+        $matches = self::next(7)->get();
+        return $matches;
+    }
 
 
     public static function getMatchs()
@@ -44,6 +71,19 @@ class Matche extends Model
             return $request->collect();
         }
         return [];
+    }
+
+    /**
+     * Scope a query to get next matches
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNext($query, $days = 0)
+    {
+        $today = Carbon::now()->toDateString();
+        $until = Carbon::now()->addDays($days)->toDateString();
+        return $query->whereDate('match_date', '>=', $today)->whereDate('match_date', '<=', $until);
     }
 
 
